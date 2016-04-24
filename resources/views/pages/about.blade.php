d3061d1795cb9194ba24d83d9207522b4a37b8f8
Monica S. Flores <monicasflores1@gmail.com>
2016-04-24 15:10:14 -0400

    learning how to use layout in order to do the layout using layout.blade.php.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,64 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Welcome to the First Version ! </title>
+@extends('layout')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('content')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                background: #557700 url("/resources/assets/cupcakebackground.jpg");
-                filter: alpha(opacity=70);
-                filter: progid: DXImageTransform.Microsoft.Alpha(opacity=30);
-                -moz-opacity: 0.70;
-                opacity: 0.7;
-                zoom: 1;
-                text-align: center;
-                display: table-cell;
-                vertical-align: top;
-                    margin-top: 100px;
-            }
-
-            .content {
-                text-align: left;
-                display: inline-block;
-                    width: 60%;
-                        margin: 0 auto;
-                            padding: 10px;
-                        color: #FFF;
-                        background-color: #000;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            a,
-            a:link,
-            a:visited,
-            a:active,
-            a:hover {
-                color: #FFF;
-                    text-decoration: underline;
-            }
-
-        </style>
-
-    </head>
-    <body>
         <div class="container">
             <div class="content">
                 <div class="title">About Us</div>
@@ -79,5 +22,7 @@
 
             </div>
         </div>
-    </body>
-</html>
+
+
+
+@stop
