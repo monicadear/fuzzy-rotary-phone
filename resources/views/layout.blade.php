c7912b841c98054ebb022e3a54546c822c14228e
Monica S. Flores <monicasflores1@gmail.com>
2016-04-24 15:15:44 -0400

    Define different yield sections so we can do custom headers and footers on each content page, depending on the page.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
+       <style>
             html, body {
                 height: 100%;
             }
@@ -57,10 +57,14 @@
 
         </style>
 
+            @yield('header')
+
     </head>
     <body>
-  
+
     		@yield('content')
+
+            @yield('footer')
 
     </body>
 </html>
